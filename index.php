192e90025886c77c505ab43774fa13f46793c465
Pojoke <pavlykdmutro@gmail.com>
2024-11-24 14:03:08 +0200

    dz2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$weathers_json = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&lang=ua&APPID=4e983a238d8dca5893648e37bbffcea1");
+$weathers_json = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=Bratislava,sk&lang=ua&APPID=4e983a238d8dca5893648e37bbffcea1");
 $weathers = json_decode($weathers_json);
 print_r($weathers);
 
